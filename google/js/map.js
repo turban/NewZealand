a596cc553a70900500606e10f57885230892a788
Bjorn Sandvik <bjorn@thematicmapping.org>
2012-08-10 18:15:58 +0200

    First commit

---
+++ google/js/map.js
@@ -0,0 +1,9 @@
+function initialize() {
+
+  var map = new google.maps.Map(document.getElementById("map"), {
+    center: new google.maps.LatLng(-41.2, 172.5),
+    zoom: 6,
+    mapTypeId: google.maps.MapTypeId.TERRAIN
+  });
+
+}
